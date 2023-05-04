ed1d762dcc0f9221db69ccf27ce460a80c7877c5
ChoiHL97 <chlgml9711@naver.com>
2023-05-05 01:54:36 +0900

    Add seed_buiding & seed_lockers

--- locker/models.py
+++ locker/models.py
@@ -3,18 +3,25 @@ from django.db import models
 from major.models import Major
 from user.models import User
 
-#Create your models here.
-
+# ----건물번호----
+# 교내에서 사용하는 건물번호를 그대로 적용, 사이버관은 원래 C로 표현하는데 임의로 5로 지정
+# 인문과학관 : 1, 교수학습개발원 : 2, 사회과학관 : 3, 국제학사 : 4, 사이버관 : 5
 class Building(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    name = models.CharField(max_length=20) # ex) 1 - 인문관, 3 - 사회과학관
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=30)
 
+    class Meta:
+        db_table = 'building'
 
+    def __str__(self):
+        return self.name
+
+#Create your models here.
 class Locker(models.Model): # 얘 만들려면 major db가 먼저 있어야 한다.
     id = models.BigAutoField(primary_key=True)
-    building_id = models.IntegerField() # 빌딩은 int로 잘 되었고,
+    # building_id = models.IntegerField() # 빌딩은 int로 잘 되었고,
     major = models.ForeignKey(Major, related_name="locker", on_delete=models.PROTECT, db_column="major") # 맞는지 모르겠다.
-    # building_id = models.ForeignKey(Building, related_name='locker', on_delete=models.PROTECT, db_column='building_id')
+    building_id = models.ForeignKey(Building, related_name='locker', on_delete=models.PROTECT, db_column='building_id')
     owned_id = models.ForeignKey(User, related_name='owned_locker', on_delete=models.PROTECT, db_column="owned_id", null=True, blank=True) # 이건 맞고 
     shared_id = models.ForeignKey(User, related_name='shared_locker', on_delete=models.PROTECT, db_column="shared_id", null=True, blank=True) # 이것도 맞다.
     is_share_registered = models.BooleanField(default=False) # 쉐어를 하겠다고 등록한 경우
--- major/management/commands/seed_majors.py
+++ major/management/commands/seed_majors.py
@@ -2,41 +2,23 @@ from django.core.management.base import BaseCommand, CommandParser
 from django_seed import Seed
 from major.models import Major
 from itertools import cycle
+import info
 
 class Command(BaseCommand):
     help = 'Seed Major data'
 
-    # def add_arguments(self, parser):
-    #     parser.add_argument(
-    #         "--total",
-    #         default=3,
-    #         type=int,
-    #         help="몇 개의 전공을 만드는지"
-    #     )
-
-    #     return
-
     def handle(self, *args, **options):
-        # total = options.get("tatal")
         seeder = Seed.seeder()
 
-        # 학과개수 32개
-        names = cycle(['프랑스어학부','독일어과','노어과','스페인어과','이탈리아어과',
-                       '포르투갈어과','네덜란드어과','스칸디나비아어과','말레이인도네시아어과','아랍어과',
-                       '태국어과','베트남어과','인도어과','터키아제르바이잔어과','페르시아어이란학과',
-                       '몽골어과','ELLT학과','영미문학문화학과','EICC','중국학대학',
-                       '일본학대학','정치외교학과','행정학과','영어교육과','한국어교육과',
-                       '프랑스어교육과','독일어교육과','중국어교육과','상경대학','경영학부',
-                       '국제학부',"LD학부"])
-
-        seeder.add_entity(Major, 32, {
-            'name': lambda x: next(names),
-            'apply_start_date': None,
-            'apply_end_date': None,
-            'priority_first': None,
-            'priority_second': None,
-            'priority_third': None,
-        })
+        for major in info.MAJORS :
+            seeder.add_entity(Major, 1, {
+                'name': major,
+                'apply_start_date': None,
+                'apply_end_date': None,
+                'priority_first': None,
+                'priority_second': None,
+                'priority_third': None,
+            })
 
         seeder.execute()
-        self.stdout.write(self.style.SUCCESS("Success!"))
+        self.stdout.write(self.style.SUCCESS("Major data, Success!"))
