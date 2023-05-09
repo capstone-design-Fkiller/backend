8559a801f982e83ed8a64ad555cccb95f84680ac
son1473 <57697373+son1473@users.noreply.github.com>
2023-05-10 03:28:08 +0900

    Complete Apply

--- apply/serializers.py
+++ apply/serializers.py
@@ -1,40 +1,30 @@
+from major.models import Major
 from .models import Apply, Priority1
 from rest_framework import serializers
 
 
 class PrioritySerializer(serializers.ModelSerializer):
-    field_type = serializers.ChoiceField(choices=[('char', 'CharField'), ('int', 'IntegerField'), ('bool', 'BooleanField')])
-    print('test1')
+    # field_type = serializers.ChoiceField(choices=[('char', 'CharField'), ('int', 'IntegerField'), ('bool', 'BooleanField')])
+    # print('test1')
+    question = serializers.CharField(source='question.priority_first')  # major 필드에 user.major.name 값을 serialize -> {major = "ELLT"} 로 출력
+    answer = serializers.CharField(source='answer.priority_1_answer')  # major 필드에 user.major.name 값을 serialize -> {major = "ELLT"} 로 출력
+
 
     class Meta:
         model = Priority1
-        fields = ['question', 'answer', 'field_type']
-    
-    # 왜 여기로 진입하지를 못할까...?
-    def to_internal_value(self, data):
-        print('test2')
-        field_type = data.get('field_type')
-        answer = data.get('answer')
-        if field_type == 'int':
-            try:
-                answer = int(answer)
-            except (ValueError, TypeError):
-                raise serializers.ValidationError('Invalid value for IntegerField')
-        elif field_type == 'bool':
-            if answer.lower() in ['true', '1']:
-                answer = True
-            elif answer.lower() in ['false', '0']:
-                answer = False
-            else:
-                raise serializers.ValidationError('Invalid value for BooleanField')
-        print(answer)
-        return {'answer': answer}
-
-
+        fields = '__all__'
 
 class ApplySerializer(serializers.ModelSerializer):
-    priority_1 = PrioritySerializer() # question answer를 담을 수 있도록
+    # priority_1 = PrioritySerializer() # question answer를 담을 수 있도록
     
+    class Meta:
+        model = Apply
+        fields = '__all__'
+        
+
+class ApplyPostSerializer(serializers.ModelSerializer):
+    # priority_1 = PrioritySerializer() # question answer를 담을 수 있도록
+
     class Meta:
         model = Apply
         fields = '__all__'
--- apply/views.py
+++ apply/views.py
@@ -1,3 +1,4 @@
+from django.forms import ValidationError
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -8,27 +9,52 @@ from apply.serializers import ApplySerializer, PrioritySerializer
 
 class PriorityAPIView(APIView):
     def get(self, request):
-        priorities = Priority1.objects.all()
-        serializer = PrioritySerializer(priorities, many=True)
-        return Response(serializer.data)
+        try:
+            if request.GET: # 쿼리 존재시, 쿼리로 필터링한 데이터 전송.
+                params = request.GET
+                params = {key: (lambda x: params.get(key))(value) for key, value in params.items()}
+                priorities = Priority1.objects.filter(**params)
+            else: # 쿼리 없을 시, 전체 데이터 요청
+                priorities = Priority1.objects.all()
+            serializer = PrioritySerializer(priorities, many=True)
+            return Response(serializer.data)
+        except ValidationError as err:
+                return Response({'detail': f'{err}'}, status=status.HTTP_400_BAD_REQUEST)
     
-    def post(self, request, format=None):
-        serializer = PrioritySerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)  # 유효성 검사 실패 시 예외 발생
-        serializer.save()  # serializer를 통해 데이터를 모델 인스턴스로 저장
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    # def post(self, request, format=None):
+    #     serializer = PrioritySerializer(data=request.data)
+    #     serializer.is_valid(raise_exception=True)  # 유효성 검사 실패 시 예외 발생
+    #     serializer.save()  # serializer를 통해 데이터를 모델 인스턴스로 저장
+    #     return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 class ApplyAPIView(APIView):
     def get(self, request):
-        applys = Apply.objects.all()
-
-        serializer = ApplySerializer(applys, many=True)
-        return Response(serializer.data)
+        try:
+            if request.GET: # 쿼리 존재시, 쿼리로 필터링한 데이터 전송.
+                params = request.GET
+                params = {key: (lambda x: params.get(key))(value) for key, value in params.items()}
+                applys = Apply.objects.filter(**params)
+            else: # 쿼리 없을 시, 전체 데이터 요청
+                applys = Apply.objects.all()
+            serializer = ApplySerializer(applys, many=True)
+            return Response(serializer.data)
+        except ValidationError as err:
+                return Response({'detail': f'{err}'}, status=status.HTTP_400_BAD_REQUEST)
     
     def post(self, request):
-        serializer = ApplySerializer(data = request.data)
+        # request.data
+        # major = priority = Priority.objects.create(question=2,answer=)
+
+        serializer = ApplySerializer(data = request.data) # json을 변환하게 된다.
         if serializer.is_valid():
-            serializer.save()
+            apply = serializer.save()
+            apply_id = apply.id
+            print(apply_id, '출력해봐~~')
+            priority1_answer = serializer.validated_data.get('priority_1_answer')
+            priority1 = Priority1.objects.create(question=serializer.validated_data['major'], answer=apply)
+            priority1.save()
+            # priority1 = Priority1.objects.create(question=serializer.validated_data['major'], answer=priority1_answer)
+            # priority1.answer = priority1_answer
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
