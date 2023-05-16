1defa022ae4d6f9bd77c72e50d4b283635539420
son1473 <57697373+son1473@users.noreply.github.com>
2023-05-16 23:48:26 +0900

    Add Static Root on settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 import my_settings
 import pymysql
@@ -183,6 +184,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
