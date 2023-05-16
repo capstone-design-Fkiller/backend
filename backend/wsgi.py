b1b2bcc25886bbadc91e61521071b4a303813a9d
Ubuntu <ubuntu@ip-172-31-34-84.ap-northeast-2.compute.internal>
2023-05-16 14:34:51 +0000

    Set up nginx and uwsgi

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+#site.addsitedir('/home/ubuntu/.local/share/virtualenvs/backend-rgN3jAOl/lib/python3.11/site-packages')
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 
 application = get_wsgi_application()
