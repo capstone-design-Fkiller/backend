7b5323e1fccf7b9697baef1259c45fabe06ebcc6
son1473 <57697373+son1473@users.noreply.github.com>
2023-05-28 00:01:23 +0900

    Make int Notice major

--- notice/serializers.py
+++ notice/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 
 
 class NoticeSerializer(serializers.ModelSerializer):
-    major = serializers.CharField(source='major.name', allow_null=True)  # major 필드에 user.major.name 값을 serialize -> {major = "ELLT"} 로 출력
+    # major = serializers.CharField(source='major.name', allow_null=True)  # major 필드에 user.major.name 값을 serialize -> {major = "ELLT"} 로 출력
 
     class Meta:
         model = Notice
