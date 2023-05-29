4f00681c24c7076a36e3875438536c89c3c31128
ChoiHL97 <chlgml9711@naver.com>
2023-05-30 01:48:30 +0900

    Major priority null 허용

--- major/serializers.py
+++ major/serializers.py
@@ -8,9 +8,15 @@ class PrioritySerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class MajorSerializer(serializers.ModelSerializer):
-    priority_1 = PrioritySerializer()
-    priority_2 = PrioritySerializer()
-    priority_3 = PrioritySerializer()
+    priority_1 = PrioritySerializer(required=False, allow_null=True)
+    priority_2 = PrioritySerializer(required=False, allow_null=True)
+    priority_3 = PrioritySerializer(required=False, allow_null=True)
+
+    class Meta:
+        model = Major
+        fields = '__all__'
+
+class MajorRequestSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Major
