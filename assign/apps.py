0052ec87133e74c6a1f8f58daa335b1a94e0a663
ChoiHL97 <chlgml9711@naver.com>
2023-06-02 06:13:39 +0900

    add : 쉐어 기간 끝나면 자동반납

--- assign/apps.py
+++ assign/apps.py
@@ -7,6 +7,9 @@ def task():
     # 이용기간이 만료된 사물함 배정정보와 신청정보 삭제
     command = "python manage.py delete_expired_data"
     subprocess.run(command, shell=True)
+    # 쉐어 기간이 끝난 사물함 반납
+    command2 = "python manage.py patch_shared_locker"
+    subprocess.run(command2, shell=True)
     pass
 
 class AssignAppConfig(AppConfig):
