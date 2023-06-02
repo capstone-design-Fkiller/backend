759b616b4ac8ebdacbe0024971024b3ab36549f2
son1473 <57697373+son1473@users.noreply.github.com>
2023-06-02 20:49:54 +0900

    배정 기준 설정 시, 신청 데이터 자동 생성

--- major/views.py
+++ major/views.py
@@ -1,11 +1,19 @@
+from django_seed import Seed
+import random
 from django.forms import ValidationError
+from faker import Faker
 from rest_framework import status, generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.http import Http404
+from apply.models import Apply
+from locker.models import Building, Locker
 
 from major.models import Major
 from major.serializers import MajorSerializer, MajorRequestSerializer
+from user.models import User
+from django.core.management import call_command
+
 
 class MajorAPIView(generics.ListCreateAPIView):
     serializer_class = MajorSerializer
@@ -49,10 +57,50 @@ class MajorDetail(generics.RetrieveUpdateDestroyAPIView):
     
     
     def patch(self, request, pk, format=None):
+        def generate_priority_answer(priority):
+                if priority in ["재학여부", "통학여부", "학생회비 납부여부"]:
+                    return random.choice([True, False])
+                elif priority == "통학시간":
+                    return random.randint(1, 150)
+                elif priority == "고학번":
+                    return random.randint(16, 23)
+                elif priority == "전공수업수":
+                    return random.randint(1, 7)
+                else:
+                    return None
+
+        lockers = Locker.objects.filter(major=pk)
+        lockers_building = lockers.values_list('building_id', flat=True).distinct()
+        print(lockers_building, '빌딩 데이터')
+
+        seeder = Seed.seeder()
+        fake = Faker()
+
+        users = User.objects.all()
+
+
         major = self.get_object(pk)
+
         serializer = MajorRequestSerializer(major, data=request.data, partial=True) 
         if serializer.is_valid():
             serializer.save()
+
+            major = self.get_object(pk)
+            for user in users :
+                seeder.add_entity(Apply, 1, {
+                    'major': lambda x: Major.objects.filter(id__in=[pk]).order_by('?').first(), # 테스트 용
+                    'user': user,
+                    'building_id': lambda x: Building.objects.filter(id__in=lockers_building).order_by('?').first(),
+                    'priority_1_answer': lambda x: generate_priority_answer(request.data["priority_1"]),
+                    'priority_2_answer': lambda x: generate_priority_answer(request.data["priority_2"]),
+                    'priority_3_answer': lambda x: generate_priority_answer(request.data["priority_3"]),
+                    'created_at': lambda x: fake.date_time_between(start_date='-3d', end_date='now')
+                })
+
+            seeder.execute()
+            print("Apply data, Success!")
+            # self.stdout.write(self.style.SUCCESS("Apply data, Success!"))
+
             return Response(serializer.data) 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
