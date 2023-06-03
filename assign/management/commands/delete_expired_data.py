abbd3298dc10834c88d4a74b793f55c21c39cbf8
son1473 <57697373+son1473@users.noreply.github.com>
2023-06-03 20:22:35 +0900

    Add: 배정 삭제 시 배정 기준 전체 삭제 기능 추가

--- assign/management/commands/delete_expired_data.py
+++ assign/management/commands/delete_expired_data.py
@@ -14,10 +14,21 @@ class Command(BaseCommand):
         for major in majors:
             # assign 데이터 전체 삭제
             AssignAPIView().delete(None, major=major.pk)
+            
+            # 배정 기준 전체 삭제
+            major.start_date = None
+            major.end_date = None
+            major.apply_start_date = None
+            major.apply_end_date = None
+            major.priority_1 = None
+            major.priority_2 = None
+            major.priority_3 = None
+            
+            major.save()
 
         # apply 데이터 전체 삭제    
         applies = Apply.objects.all()
         for apply in applies:
-            ApplyDetail().delete(None, pk=apply.id)
+            ApplyDetail().delete(None, pk=apply.id)        
 
         self.stdout.write(self.style.SUCCESS("Delete Apply & Assign Data!"))
